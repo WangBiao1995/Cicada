088c25152a924b5a5c9fdc8d33c71a3413b51300
unknown <2935615971@qq.com>
2024-05-27 14:39:01 +0800

    Setup basic Application and Entry Point

---
+++ Cicada/src/Hazel/Core.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef HZ_PLATFORM_WINDOWS
+	#ifdef HZ_BUILD_DLL
+	   #define HAZEL_API _declspec(dllexport)
+	#else // HZ_BUILD_DLL
+       #define HAZEL_API _declspec(dllimport)
+    #endif
+
+#else
+  #error Hazel only support Windows!
+
+#endif
